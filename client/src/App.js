da3be473815e20461ff7b395b867f5ba1abcf5cf
Adrian Marimon <adrianmarimon1@gmail.com>
2019-05-06 15:09:52 -0400

    removed comented code

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Add from './pages/Add'
 import Collection from './pages/Collection'
 import Contact from './pages/Contact';
-// class App extends React.Component {
-//   state = { serverMessage: '' }
-
-//   componentDidMount(){
-//     fetch('/api/demo')
-//       .then(response => response.json())
-//       .then(data => this.setState({ serverMessage: data.message }))
-//   }
-
-//   render(){
-//     return (
-//       <div id="demo">
-//         <h1>Hello from client/src/App.js</h1>
-//         <h1>{this.state.serverMessage}</h1>
-//       </div>
-//     )
-//   }
-// }
 
 const App = () => {
   return (
