3cad9280d9f554a336fd2f59db3a07a26a4d5f75
Laz <lquin008@gmail.com>
2019-05-07 20:34:44 -0400

    added delete functionality to the collection page and server

--- client/src/page/Collection.js
+++ client/src/page/Collection.js
@@ -12,22 +12,44 @@ class Collection extends React.Component {
       axios.get(`/api/collection`).then(res => this.setState({ albums:  res.data }))
     }
 
+    deleteAlbum = (id) =>{
+          axios.delete(`/api/collection/${id}`)
+          .then(response => {
+             if(response.status === 202){
+              let {albums} = this.state
+              albums = albums.filter(album => album.id !== id)
+              this.setState({albums})
+             }
+          })
+          .catch(err => console.log(err))    
+  }
+
   render(){
       return(
         <>
         <Navbar/>
     <div id="collection">
       <h1 id="collection-header">My <span id="wm-collection">WynMusic</span> Collection</h1>
+           {
+            !this.state.albums.length && 
+            <>
+              <div id="empty-message">
+                Your Record Collection is empty. <br/> Start adding to it below!
+              </div>
+              <Link to="/search"><button id="search-collection">Search for Albums</button></Link>
+            </>
+            }
       <div id="collection-body">
         <table draggable="false">
           <tbody>
             {
               this.state.albums.map(album => (
                 <tr id="album-row">
-                  <td id="collection-image">{album.images && <img src={album.images[2].url} className="album-cover-collection" alt='album img' />}</td>
+                  <td id="collection-image">{album.images && <img src={album.images[2].url}             className="album-cover-collection" alt='album img' />}</td>
                   <td id="collection-artist">{album.artists && album.artists[0].name}</td>
                   <td id="collection-album">{album.name}</td>
-                  <td id="button-column"><button id="collection-button"><Link to={`/albums/${album.id}`} target="_blank">Details</Link></button></td>
+                  <td id="button-column"><button className="collection-button"><Link to={`/albums/${album.id}`} target="_blank">Details</Link></button></td>
+                  <td id="remove-column"><button className="collection-button" onClick={() => this.deleteAlbum(album.id)}>Remove</button></td>
                 </tr>
               ))
             }
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.static('public'))
 const req = require('request') //required request to perform http requests to the api, named req because there are more than 1 request param/variable
 
 const albumCollection = []
+let removeId = 0
 
 const client_id = process.env.CLIENT_ID
 const client_secret = process.env.CLIENT_SECRET
@@ -27,7 +28,7 @@ let authOptions = { //this object exsists just to set the token for use in the c
 
 app.get('/api/search/:query', (request, res) => { //this is using the express server to ping the spotify demo to get the requested JSON info, using the token - for SEARCH page
 
-  req.post(authOptions, function(error, response, body) {
+  req.post(authOptions, function (error, response, body) {
     if (!error && response.statusCode === 200) {
 
       var token = body.access_token;
@@ -38,54 +39,71 @@ app.get('/api/search/:query', (request, res) => { //this is using the express se
         },
         json: true
       };
-      req.get(options, function(error, response, body) {
-          res.json(body.albums.items)
+      req.get(options, function (error, response, body) {
+        res.json(body.albums.items)
       });
     }
   });
 })
 
-  app.get('/api/albums/:id', (request, res) => { //this is the GET for DETAILS page
-  
-    req.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-    
-        var token = body.access_token;
-        var options = {
-          url: `https://api.spotify.com/v1/albums/${request.params.id}`,
-          headers: {
-            'Authorization': 'Bearer ' + token
-          },
-          json: true
-        };
-        req.get(options, function(error, response, body) {
-            res.json(body)
-        });
-      }
-    });
+app.get('/api/albums/:id', (request, res) => { //this is the GET for DETAILS page
+
+  req.post(authOptions, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+
+      var token = body.access_token;
+      var options = {
+        url: `https://api.spotify.com/v1/albums/${request.params.id}`,
+        headers: {
+          'Authorization': 'Bearer ' + token
+        },
+        json: true
+      };
+      req.get(options, function (error, response, body) {
+        res.json(body)
+      });
+    }
+  });
 })
 
-//////////////////////////////////CODE for building your collection//////////////////////////////
+//////////////////////////////////CODE for building / deleting your collection//////////////////////////////
 
-app.get('/api/collection', (request, response)=>{
+app.get('/api/collection', (request, response) => {
   response.json(albumCollection)
 })
 
-app.post('/api/collection', (request, response)=>{
+app.post('/api/collection', (request, response) => {
   const addedAlbum = request.body
-  if(addedAlbum.album_type){
-      albumCollection.push(addedAlbum)
-      response.json(addedAlbum)
+  if (addedAlbum.album_type) {
+    albumCollection.push(addedAlbum)
+    response.json(addedAlbum)
   } else {
-      response.status(422).json({error: "album info required"})
-}})
+    response.status(422).json({
+      error: "album info required"
+    })
+  }
+})
 
-//////////////////////////////////CODE for building your collection//////////////////////////////
+app.delete('/api/collection/:id', (request, response) => {
+
+  const album = albumCollection.find(album => album.id === request.params.id)
+  if (album && album.album_type) {
+    const index = albumCollection.findIndex(e => e.id == album.id)
+    albumCollection.splice(index, 1)
+    response.status(202).json({
+      success: `deleted ${album.id}`
+    })
+  } else {
+    response.status(204)
+  }
+})
+
+//////////////////////////////////CODE for building /deleting your collection//////////////////////////////
 
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  
+
   app.use(express.static(path.join(__dirname, 'client/build')))
   // Handle React routing, return all requests to React app
   app.get('*', (request, response) => {
@@ -96,5 +114,7 @@ if (process.env.NODE_ENV === 'production') {
 const port = process.env.PORT || 8080
 app.listen(
   port,
-  () => { console.log(`API listening on port ${port}...`) }
+  () => {
+    console.log(`API listening on port ${port}...`)
+  }
 )
